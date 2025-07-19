87d8933334ddb5b43a4a9166c34b9469f9195d79
pratikraut2 <pratik.raut180@gmail.com>
2025-07-19 17:58:46 +0530

    some changes

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
-from django.core.mail import send_mail, EmailMessage
+from django.core.mail import  EmailMessage
 from decouple import config
 
 def home(request):
